acdcdbed7d7b418f0bfc302f92999bdcf1243ff1
Wesley <weslley182@gmail.com>
2016-10-23 21:39:47 -0200

    COntinução projeto

---
+++ ArquiteturaNovo.Infra.Data/Context/ProjModeloContext.cs
@@ -0,0 +1,22 @@
+﻿using ArquiteturaNovo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquiteturaNovo.Infra.Data.Context
+{
+    public class ProjModeloContext: DbContext
+    {
+        public ProjModeloContext()
+            : base("ProjModelo")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set}
+
+    }
+}
